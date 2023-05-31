44ca9c03024a0ea098de59266868ff77ecb7be38
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-05-31 13:56:29 +0200

    feat: create_module

--- variables.tf
+++ variables.tf
@@ -1,29 +1,3 @@
-variable "subscription" {
-  type = map(object({
-    topic_name    = string
-    filter_policy = optional(string)
-  }))
-  description = "The subscription details such as topic name and filter policy."
-  default     = {}
-}
-
-variable "subscription_aws_account_id" {
-  type        = string
-  description = "The AWS account ID for the subscription."
-}
-
-variable "dlq_enabled" {
-  type        = bool
-  description = "Defines if Dead Letter Queue (DLQ) is enabled."
-  default     = true
-}
-
-variable "dlq_max_receive_count" {
-  type        = number
-  description = "The maximum number of times a message can be received from the DLQ before it's discarded."
-  default     = 5
-}
-
 variable "alarm" {
   type = object({
     datapoints_to_alarm = optional(number, 3)
@@ -47,4 +21,30 @@ variable "alarm_topic_arn" {
   type        = string
   description = "The ARN of the SNS Topic used for notifying about alarm/ok messages."
   default     = null
+}
+
+variable "dlq_enabled" {
+  type        = bool
+  description = "Defines if Dead Letter Queue (DLQ) is enabled."
+  default     = true
+}
+
+variable "dlq_max_receive_count" {
+  type        = number
+  description = "The maximum number of times a message can be received from the DLQ before it's discarded."
+  default     = 5
+}
+
+variable "subscription" {
+  type = map(object({
+    topic_name    = string
+    filter_policy = optional(string)
+  }))
+  description = "The subscription details such as topic name and filter policy."
+  default     = {}
+}
+
+variable "subscription_aws_account_id" {
+  type        = string
+  description = "The AWS account ID for the subscription."
 }
