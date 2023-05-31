0d532e5a61466738b90eb616158345fc27c88f20
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-05-31 12:35:40 +0200

    feat: create_module

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+data "aws_iam_policy_document" "subscription" {
+  count = length(var.subscription) >= 1 ? 1 : 0
+
+  statement {
+    actions   = ["sqs:SendMessage"]
+    resources = [module.sqs.queue_arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.subscription_aws_account_id}:root"]
+    }
+
+    dynamic "condition" {
+      for_each = var.subscription
+      content {
+        test     = "ArnEquals"
+        values   = [for key in keys(aws_sns_topic_subscription.default) : aws_sns_topic_subscription.default[key].topic_arn]
+        variable = "aws:SourceArn"
+      }
+    }
+  }
+}
+
 module "sqs" {
   source  = "terraform-aws-modules/sqs/aws"
   version = "4.0.1"
@@ -8,10 +31,8 @@ module "sqs" {
 
   queue_policy_statements = {
     account = {
-      sid = "AccountReadWrite"
       actions = [
-        "sqs:SendMessage",
-        "sqs:ReceiveMessage",
+        "*",
       ]
       principals = [
         {
@@ -22,44 +43,12 @@ module "sqs" {
     }
   }
 
+  source_queue_policy_documents = [try(data.aws_iam_policy_document.subscription[0].json, null)]
+
   create_dlq = var.dlq_enabled
 
   redrive_policy = {
     maxReceiveCount = var.dlq_max_receive_count
-  }
-
-  tags = module.this.tags
-}
-
-module "sqs_subscription" {
-  for_each = var.subscription
-  source   = "terraform-aws-modules/sqs/aws"
-  version  = "4.0.1"
-
-  name = module.this.id
-
-  create = false
-
-  create_queue_policy = true
-  queue_policy_statements = {
-    "each.value.topic_name" = {
-      sid = "SubscriptionReadWrite"
-      actions = [
-        "sqs:SendMessage",
-        "sqs:ReceiveMessage",
-      ]
-      principals = [
-        {
-          type        = "AWS"
-          identifiers = ["arn:aws:iam::${var.subscription_aws_account_id}:root"]
-        }
-      ]
-      condition = {
-        test     = "ArnEquals"
-        values   = "arn:aws:sns:${module.this.aws_region}:${var.subscription_aws_account_id}:${each.value.topic_name}"
-        variable = "aws:SourceArn"
-      }
-    }
   }
 
   tags = module.this.tags
