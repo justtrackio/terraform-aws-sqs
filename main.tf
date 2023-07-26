2f5d26a891af10af8317a9e03dc3eac6a33b928f
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-07-26 17:01:43 +0200

    feat: create module (#3)

---
+++ main.tf
@@ -0,0 +1,58 @@
+data "aws_iam_policy_document" "subscription" {
+  count = length(var.subscription) >= 1 ? 1 : 0
+
+  statement {
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage",
+    ]
+
+    resources = [module.sqs.queue_arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    dynamic "condition" {
+      for_each = var.subscription
+      content {
+        test     = "ArnEquals"
+        values   = [for key in keys(aws_sns_topic_subscription.default) : aws_sns_topic_subscription.default[key].topic_arn]
+        variable = "aws:SourceArn"
+      }
+    }
+  }
+}
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "4.0.1"
+
+  name = module.this.id
+
+  create_queue_policy = length(var.subscription) >= 1
+
+  source_queue_policy_documents = try([data.aws_iam_policy_document.subscription[0].json], [])
+
+  create_dlq = var.dlq_enabled
+
+  redrive_policy = {
+    maxReceiveCount = var.dlq_max_receive_count
+  }
+
+  tags = module.this.tags
+
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+
+}
+
+resource "aws_sns_topic_subscription" "default" {
+  for_each                        = var.subscription
+  topic_arn                       = "arn:aws:sns:${module.this.aws_region}:${var.subscription_aws_account_id}:${each.value.topic_name}"
+  confirmation_timeout_in_minutes = "1"
+  endpoint_auto_confirms          = "false"
+  protocol                        = "sqs"
+  endpoint                        = module.sqs.queue_arn
+  filter_policy                   = each.value.filter_policy
+}
---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "alarm" {
+  type = object({
+    datapoints_to_alarm = optional(number, 3)
+    description         = optional(string, null)
+    evaluation_periods  = optional(number, 3)
+    backlog_minutes     = optional(number, 3)
+    period              = optional(number, 60)
+    threshold           = optional(number, 0)
+  })
+  description = "The details of the alarm such as datapoints to alarm, evaluation periods, backlog minutes, period, and threshold."
+  default     = {}
+}
+
+variable "alarm_enabled" {
+  type        = bool
+  description = "Defines if the alarm should be created."
+  default     = false
+}
+
+variable "alarm_topic_arn" {
+  type        = string
+  description = "The ARN of the SNS Topic used for notifying about alarm/ok messages."
+  default     = null
+}
+
+variable "dlq_enabled" {
+  type        = bool
+  description = "Defines if Dead Letter Queue (DLQ) is enabled."
+  default     = true
+}
+
+variable "dlq_max_receive_count" {
+  type        = number
+  description = "The maximum number of times a message can be received from the DLQ before it's discarded."
+  default     = 5
+}
+
+variable "subscription" {
+  type = map(object({
+    topic_name    = string
+    filter_policy = optional(string)
+  }))
+  description = "The subscription details such as topic name and filter policy."
+  default     = {}
+}
+
+variable "subscription_aws_account_id" {
+  type        = string
+  description = "The AWS account ID for the subscription."
+  default     = null
+}
+
+variable "visibility_timeout_seconds" {
+  description = "The visibility timeout for the queue. An integer from 0 to 43200 (12 hours)"
+  type        = number
+  default     = null
+}
